5da4a30fc887d9e6c9f528275a800ac5a3d61e0e
daviiddd18 <davidespinossanz@gmail.com>
2024-01-22 17:21:40 +0100

    Actulización

--- src/app/libros/detalles/[id].tsx
+++ src/app/libros/detalles/[id].tsx
@@ -1,10 +1,1 @@
-import { useRouter } from 'next/router';
 
-const LibroDetalle = () => {
-  const router = useRouter();
-  const { id } = router.query;
-
-  return 
-}
-
-export default LibroDetalle;
--- src/app/usuarios/registrar/page.tsx
+++ src/app/usuarios/registrar/page.tsx
@@ -1,17 +1,32 @@
 'use client'
 
+import { useState } from 'react'
 import '../../styles/globals.css'
 
 export default function Registrar (){
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    function handleFormSubmit(ev: React.FormEvent<HTMLFormElement>){
+        ev.preventDefault();
+        fetch('/api/register', {
+            method:'POST',
+            body: JSON.stringify({email, password}),
+            headers:{'Content-Type': 'application/json'}
+        })
+    }
     
     return(
-        
         <>
         <h1>Hola desde Registrar</h1>
-        <form>
-            <input type="email" placeholder="email"/>
-            <input type="password" placeholder="password"/>
-            <input type="submit" value="Registrar"/> 
+        <form className='block max-w-xs mx-auto' onSubmit={handleFormSubmit}>
+            <label htmlFor="email" className='font-bold'>Email</label>
+            <input id="email" type="email" placeholder="email" value={email} onChange={ev => setEmail(ev.target.value)}/>
+
+            <label htmlFor="password" className='font-bold'>Password</label>
+            <input id="password" type="password" placeholder="password" value={password} onChange={ev => setPassword(ev.target.value)}/>
+
+            <button type="submit">Registrar</button> 
         </form>
         </>
 
